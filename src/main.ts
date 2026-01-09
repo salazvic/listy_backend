6d52c22135e5225be834e33fafbf23d190b88143
salazvic <sadelto3011@gmail.com>
2026-01-09 14:10:13 -0300

    railway config

--- src/main.ts
+++ src/main.ts
@@ -37,7 +37,7 @@ async function bootstrap() {
 
   await app.listen(process.env.PORT ?? 3001);
 
-  console.log(`Application is running on: ${await app.getUrl()}`);
+  console.log(`Application is running on: ${process.env.PORT}`);
 
 }
 bootstrap();
